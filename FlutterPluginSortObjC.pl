4913cfa832df9a38a21ad0127622f273b036d8a9
CJ Lin <ejames.lin@gmail.com>
2020-09-02 22:32:51 +0800

    Apply perl subroutine

--- FlutterPluginSortObjC.pl
+++ FlutterPluginSortObjC.pl
@@ -2,37 +2,64 @@
 use strict;
 use warnings;
 
-my @moveToLast = @ARGV;
-
-my $filename = "Runner/GeneratedPluginRegistrant.m";
-open(FH, '<', $filename) or die $!;
-my @lines = <FH>;
-close(FH);
-
-my @plugins;
-my @output;
-
-foreach (@lines){
-	if (/^\s*}\s*$/) {
-		@plugins = sort @plugins;
-
-		foreach my $name (@moveToLast) {
-			my ($pluginFound) = grep(/$name registerWithRegistrar:/, @plugins);
-			if (defined $pluginFound) {
-				@plugins = grep($_ ne $pluginFound, @plugins);
-				push @plugins, $pluginFound;
-			}
-		}
-
-		push @output, @plugins;
-		push @output, $_;
-	} elsif (/\[\w+ registerWithRegistrar:/) {
-		push @plugins, $_;
-	} else {
-		push @output, $_;
-	}
+sub Main {
+	my $moveToLast = &HandleARGV;
+	my $filename = "Runner/GeneratedPluginRegistrant.m";
+	my @lines = &Parse($filename);
+	my @output = &AdjustOrder(\@lines, $moveToLast);
+	&Output($filename, \@output);
 }
 
-open(FH, '>', $filename) or die $!;
-print FH @output;
-close(FH);
+sub HandleARGV {
+	my $moveToLast;
+	if (scalar(@ARGV) == 2 && shift @ARGV eq "-last") {
+		$moveToLast = shift @ARGV;
+	}
+	print "Target to last: ", $moveToLast, "\n";
+	return $moveToLast;
+}
+
+sub AdjustOrder {
+	my ($linesRef, $moveToLast) = @_;
+	my @plugins;
+	my @output;
+	foreach (@$linesRef){
+		if (/^\s*}\s*$/) {
+			@plugins = sort @plugins;
+	
+			if (defined $moveToLast) {
+				my ($pluginFound) = grep(/$moveToLast registerWithRegistrar:/, @plugins);
+				print "Found: ", $pluginFound;
+				if (defined $pluginFound) {
+					@plugins = grep($_ ne $pluginFound, @plugins);
+					push @plugins, $pluginFound;
+				}
+			}
+						
+			push @output, @plugins;
+			push @output, $_;
+		} elsif (/\[\w+ registerWithRegistrar:/) {
+			push @plugins, $_;
+		} else {
+			push @output, $_;
+		}
+	}
+	return @output;
+}
+
+sub Parse {
+	my ($filename) = @_;
+	open(FH, '<', $filename) or die $!;
+	my @lines = <FH>;
+	close(FH);
+	return @lines;
+}
+
+sub Output {
+	my ($filename, $outputRef) = @_;
+	open(FH, '>', $filename) or die $!;
+	print FH @$outputRef;
+	close(FH);
+}
+
+&Main;
